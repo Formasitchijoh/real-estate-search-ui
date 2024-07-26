abed8128c7049db0e0e931e557b597105c79c8c5
Formasitchijoh <formasitf@gmail.com>
2024-07-26 05:22:23 +0100

    feat: fix login and recommendation

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import React, { useEffect, useState } from "react";
 import NavMenu from "../components/navbar";
@@ -15,14 +15,14 @@ import Link from "next/link";
 import PropertyCard from "../components/propertyCard";
 import Button from "../components/button";
 import { useRouter } from "next/navigation";
-const Home = () => { 
+const Home = () => {
   const [listings, setlistings] = useState<Array<any>>();
   const [scores, setScores] = useState<Array<any>>();
   const [query, setQuery] = useState("");
-  const [search, setSearch] = useState(false)
+  const [search, setSearch] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
-  const [newRecommendation, setNewRecommendation] = useState(false)
+  const [newRecommendation, setNewRecommendation] = useState(false);
   const [recommendations, setRecommendations] = useState<Array<any>>();
   const [usersRecommendation, setUsersRecommendation] = useState<any>();
 
@@ -67,13 +67,13 @@ const Home = () => {
             console.log("\n\nresult\n\n", result);
           });
 
-          fetch(`http://127.0.0.1:8000/api/recommendations/rec?user_id=${id}`, {
-            method: "GET",
-          })
-            .then((response) => response.json())
-            .then((result) => {
-              setUsersRecommendation(result);
-            });
+        fetch(`http://127.0.0.1:8000/api/recommendations/rec?user_id=${id}`, {
+          method: "GET",
+        })
+          .then((response) => response.json())
+          .then((result) => {
+            setUsersRecommendation(result);
+          });
       }
     } catch (error) {
       console.log(error);
@@ -81,78 +81,88 @@ const Home = () => {
   }, [currentPage]);
   return (
     <div className="">
-      <HeroSection/>
-      <CompaniesSection/>
-      <AboutSection/>
-      <div className='w-[100vw] mx-auto py-10 md:py-16 xl:py-20 flex flex-col justify-center items-center'>
-        <Button secondary text='Properties' className=' mb-4 xl:mb-6'/>
-       
-        <h2 className="text-2xl md:text-4xl lg:text-5xl text-black font-medium tracking-tight leading-snug">
-        Properties in your favorite area  </h2>
+      <HeroSection />
+      <CompaniesSection />
+      <AboutSection />
+      <div className="w-[100vw] mx-auto py-10 md:py-16 xl:py-20 flex flex-col justify-center items-center">
+        <Button secondary text="Properties" className=" mb-4 xl:mb-6" />
 
-        <div className='grid w-[100%] sm:w-[90%] md:grid-cols-2 px-4 md:px-0 lg:grid-cols-3 py-16 gap-6'>
-        {listings &&
-          listings?.slice(0, 6)?.map((listing, index) => (
-            <Link key={index} href={`/properties/${listing.listing ? listing.listing : listing.id}`}>
-              <PropertyCard
-                image={ listing.images ? listing.images[0] : listing.listing_image[0]?.image}
-                title={listing.title}
-                bedrooms={listing.bedroom}
-                bathrooms={listing.bathrooms}
-                price={listing.price}
-                location={`${listing.town}, ${listing.location} `}
-                reactions={listing.reactions}
-                pricepermonth={listing.pricepermonth}
-                listing_type={listing.listing_type}
-              />
-            </Link>
-          ))}
+        <h2 className="text-2xl md:text-4xl lg:text-5xl text-black font-medium tracking-tight leading-snug">
+          Properties in your favorite area{" "}
+        </h2>
+
+        <div className="grid w-[100%] sm:w-[90%] md:grid-cols-2 px-4 md:px-0 lg:grid-cols-3 py-16 gap-6">
+          {listings &&
+            listings?.slice(0, 6)?.map((listing, index) => (
+              <Link
+                key={index}
+                href={`/properties/${
+                  listing.listing ? listing.listing : listing.id
+                }`}
+              >
+                <PropertyCard
+                  image={
+                    listing.images
+                      ? listing.images[0]
+                      : listing.listing_image[0]?.image
+                  }
+                  title={listing.title}
+                  bedrooms={listing.bedroom}
+                  bathrooms={listing.bathrooms}
+                  price={listing.price}
+                  location={`${listing.town}, ${listing.location} `}
+                  reactions={listing.reactions}
+                  pricepermonth={listing.pricepermonth}
+                  listing_type={listing.listing_type}
+                />
+              </Link>
+            ))}
         </div>
         {usersRecommendation && (
-        <>
-          <div className="md:py-10 w-[100%] sm:w-[90%] px-4 lg:px-0 lg:w-[90%] mx-auto ">
-          <h2 className="text-2xl md:text-4xl lg:text-5xl text-black font-medium tracking-tight leading-snug">
-          People Also Like{" "}
-            </h2>
-          </div>
+          <>
+            <div className="md:py-10 w-[100%] sm:w-[90%] px-4 lg:px-0 lg:w-[90%] mx-auto ">
+              <h2 className="text-2xl md:text-4xl lg:text-5xl text-black font-medium tracking-tight leading-snug">
+                People Also Like{" "}
+              </h2>
+            </div>
 
-          <div className="grid w-full  lg:w-[90vw] mx-auto py-8 md:gap-4  flex-col md:flex-row md:grid-cols-2 px-4 lg:px-0 lg:grid-cols-3 pb-16 gap-6">
-            {usersRecommendation &&
-              usersRecommendation?.map((listing, index) => (
-                <Link
-                  key={index}
-                  href={`/properties/${
-                    listing.listing ? listing.listing : listing.id
-                  }`}
-                >
-                  <PropertyCard
-                    image={
-                      listing.listing_image
-                        ? listing.listing_image[0]?.image
-                        : listing.images[0]
-                    }
-                    title={listing.title}
-                    bedrooms={listing.bedroom}
-                    bathrooms={listing.bathrooms}
-                    price={listing.price}
-                    location={`${listing.town}, ${listing.location} `}
-                    reactions={listing.reactions}
-                    pricepermonth={listing.pricepermonth}
-                    listing_type={listing.listing_type}
-
-                  />
-                </Link>
-              ))}
-          </div>
-        </>
-      )}
+            <div className="grid w-full  lg:w-[90vw] mx-auto py-8 md:gap-4  flex-col md:flex-row md:grid-cols-2 px-4 lg:px-0 lg:grid-cols-3 pb-16 gap-6">
+              {usersRecommendation &&
+                usersRecommendation?.map((listing, index) => (
+                  <Link
+                    key={index}
+                    href={`/properties/${
+                      listing.listing ? listing.listing : listing.id
+                    }`}
+                  >
+                    <PropertyCard
+                      image={
+                        listing.listing_image
+                          ? listing.listing_image[0]?.image
+                          : listing.images[0]
+                      }
+                      title={listing.title}
+                      bedrooms={listing.bedroom}
+                      bathrooms={listing.bathrooms}
+                      price={listing.price}
+                      location={`${listing.town}, ${listing.location} `}
+                      reactions={listing.reactions}
+                      pricepermonth={listing.pricepermonth}
+                      listing_type={listing.listing_type}
+                    />
+                  </Link>
+                ))}
+            </div>
+          </>
+        )}
         <div className="flex w-full gap-3 justify-center items-center">
-            <Button quatenary text="Explore Properties" />
-            <Button icon text="Contact Agent" />
-          </div>
-    </div>      <HowItWorksSection/>
-      <Neighboorhoods/>
-      <WorkWithUs/>
+          <Button quatenary text="Explore Properties" />
+          <Button icon text="Contact Agent" />
+        </div>
+      </div>{" "}
+      {/* <HowItWorksSection /> */}
+      <Neighboorhoods />
+      <WorkWithUs />
       {/* <Testimonials/> */}
     </div>
   );
